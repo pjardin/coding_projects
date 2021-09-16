53f30205ab71f0d44a61208f1b57ac7e77a1d141
Pascal Jardin <pjardin@me.com>
2021-09-15 18:28:06 -0700

    clean up helper script

--- 2021_September/code/mixamoCleanUp.py
+++ 2021_September/code/mixamoCleanUp.py
@@ -4,18 +4,33 @@ import json
 
 import shutil
 
-saveTo = "/Users/pascaljardin/Documents/GitHub/coding_projects/2021_September/Mixamo"
-downlaods_folder = "/Users/pascaljardin/Downloads"
-
+saveTo = "/Users/pascaljardin/Documents/Mixamo_data/web_scraped/Mixamo"
 
 saved_folders = os.listdir(saveTo)
-
-for i in range(0,2531):
+print("\n\n\n========\n\n")
+for i in range(0,2441):
+    #print(i)
     path = os.path.join(saveTo, str(i))
 
     contents = os.listdir(path)
-
-    if (len(contents) != 3):
+    
+    
+    
+    s = ""
+    """
+    try:
+        os.remove(os.path.join(path, '.DS_Store'))
+        print("remove")
+    except:
+        s = "s"
+    """
+    found = False;
+    for file in contents:
+        if file.endswith(".fbx"):
+            found = True
+            break
+            
+    if (found == True):
         
         print(path)
         print(len(contents))
